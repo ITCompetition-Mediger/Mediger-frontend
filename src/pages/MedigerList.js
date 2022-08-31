dcf3f69e4638a7c5533504f7b64d8508744bb2cb
mongdiwoo <1206jiwoo@gmail.com>
2022-08-31 15:34:53 +0900

    0831 styled-components test

--- src/pages/MedigerList.js
+++ src/pages/MedigerList.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+// 변수 이름은 무조건 대문자로 시작!
+let Exh1 = styled.h1`
+  font-size: 50px;
+  color: red;
+  margin: 0px;
+  padding: 0px;
+`;
+
+const MedigerListAll = styled.div`
+  background: yellow;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+`;
 
 function MedigerList() {
   return (
-    <div>
-      <h1>Mediger List Page!</h1>
-      <h2>plus</h2>
-      <Link to="/"></Link>
-    </div>
+    <MedigerListAll>
+      <div>
+        <Exh1>Mediger List</Exh1>
+        <h2>plus</h2>
+        <Link to="/"></Link>
+      </div>
+    </MedigerListAll>
   );
 }
 
