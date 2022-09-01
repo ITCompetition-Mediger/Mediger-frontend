b05f805de7409dee2f721796de9633aee47673eb
mongdiwoo <1206jiwoo@gmail.com>
2022-09-01 21:03:27 +0900

    0901 충돌 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '././pages/Home';
-// import Header from '././components/Header';
 
 function App() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Header from '../components/Header';
 
 function Home() {
   return (
     <div>
-      <Header />
-      <h1>여기는 HOME</h1>
-      <h1>여기는 HOME</h1>
       <h1>여기는 HOME</h1>
       <Link to="/"></Link>
     </div>
