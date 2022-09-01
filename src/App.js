f20cae77ab3de66d89028363268585af6c0f162c
mongdiwoo <1206jiwoo@gmail.com>
2022-09-01 14:44:14 +0900

    0901 header 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '././pages/Home';
-import Header from '././components/Header';
+// import Header from '././components/Header';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Header" element={<Header />} />
     </Routes>
   );
 }
