0e260d3bf4b6d7b5dbcd58ab428d38a8560477fb
yujiah-github <cil05265@naver.com>
2022-09-05 01:10:17 +0900

    Feat: 의약품 검색 실패 시, SearchBar 밑에 실패 메세지 추가

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useState } from "react";
 import { HiSearch } from "react-icons/hi";
 
 const SearchBarWrapper = styled.div`
+    padding-top: 10px;
+    display: flex;
+    justify-content: center;
 
     input{
         border-radius: 30px;
@@ -19,25 +22,61 @@ const SearchBarWrapper = styled.div`
         }
     }
 
-    .searchIcon {
+    svg {
+        padding-top: 15px;
         color: #3A6C60;
         font-size: 35px;
-        padding: 30px;
     }
 `;
 
-
-
 function SearchBar(){
+    const [inputValue, setInputValue] = useState("");
+
+    const inputHandler = (event) => {
+        setInputValue(event.currentTarget.value);
+    };
+
+    const goPillSearch = (event) => {
+        event.preventDefault();
+
+        //추후 수정 필요
+        if(inputValue==" "){
+        document.getElementById('checkInputValue').innerHTML='<b>검색어를 다시 확인해주세요.<b>';
+        document.getElementById('checkInputValue').style.fontSize='15px';
+        document.getElementById('checkInputValue').style.color='red';
+        document.getElementById('checkInputValue').style.textAlign='center';
+        document.getElementById('checkInputValue').style.paddingTop='10px';
+        }
+
+        /*
+        else if(inputValue==null){
+        document.getElementById('checkInputValue').innerHTML='<b>검색어를 다시 확인해주세요.<b>';
+        document.getElementById('checkInputValue').style.color='red';
+        }
+        */
+
+        //추후 삭제 필요
+        else{
+            alert('검색완료');
+        }
+    }
+
     return(
-        <SearchBarWrapper>
-            <input
-                placeholder="의약품명 및 증상 검색"
-            />
-            <span className="searchIcon">
-                <HiSearch /> 
-            </span>
-        </SearchBarWrapper>
+        <>
+            <SearchBarWrapper>
+                <input
+                    placeholder="의약품명 및 증상 검색"
+                    value={inputValue}
+                    onChange={inputHandler}
+                />
+                <span className="searchIcon">
+                    <HiSearch
+                        onClick={goPillSearch}
+                    /> 
+                </span>
+            </SearchBarWrapper>
+            <div id="checkInputValue"></div>
+        </>
     );
 }
 
