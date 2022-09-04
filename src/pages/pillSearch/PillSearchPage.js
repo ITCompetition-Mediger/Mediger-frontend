e832c6420a739f7c39e222095cb1eef89f8dadb2
yujiah-github <cil05265@naver.com>
2022-09-04 19:28:52 +0900

    Feat: PillSearchPage에 SearchBar, NavBar 추가

--- src/pages/pillSearch/PillSearchPage.js
+++ src/pages/pillSearch/PillSearchPage.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "../../components/Header";
+import NavBar from "../../components/NavBar";
+import SearchBar from "../../components/SearchBar";
 import PillSearchList from "./PillSearchList";
 
 const PillSearchWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
     .pillBox{
-        white-space: nowrap;
         color: #3C7466;
+        padding-bottom: 10px;
     }
 `;
 
@@ -14,11 +20,14 @@ function PillSearch(){
     return(
         <PillSearchWrapper>
             <Header />
+            <SearchBar />
+            <div>검색어를 다시 입력해주세요</div>
             <div className="pillBox">
                 전체 1개 
                 <hr className="horizontal"></hr>
             </div>
             <PillSearchList />
+            <NavBar />
         </PillSearchWrapper>
     );
 }
