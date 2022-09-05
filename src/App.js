cd82779ceea1576d9588a0b1588e43cdb869d657
yujiah-github <cil05265@naver.com>
2022-09-05 21:43:54 +0900

    Feat: App.js에 React Router 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '././pages/Home';
 import MedigerList from './pages/MedigerList';
+import PillSearchPage from './pages/pillSearch/PillSearchPage';
+import PillSearchPlusPage from './pages/pillSearch/PillSearchPlusPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/MedigerList" element={<MedigerList />} />
+      <Route path="/pillSearch" element={<PillSearchPage />} />
+      <Route path="/pillSearch/detail" element={<PillSearchPlusPage />} />
     </Routes>
   );
 }
