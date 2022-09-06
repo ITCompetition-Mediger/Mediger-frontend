e0efdfad36fef250b889033f78de9b2ff4e58f39
yujiah-github <cil05265@naver.com>
2022-09-07 01:46:47 +0900

    Feat: LoginButton Component 생성

---
+++ src/pages/login/LoginButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import GoogleLoginButton from './GoogleLoginButton';
+import KakaoLoginButton from './KaKaoLoginButton';
+import NaverLoginButton from './NaverLoginButton';
+import styled from 'styled-components';
+
+const Wrapper = styled.form`
+    display: flex;
+    flex-direction: column;
+    padding-left: 2.5rem;
+    margin-top: 1.5rem;
+`;
+
+function LoginButton(){
+    return(
+        <Wrapper>
+            <KakaoLoginButton />
+            <GoogleLoginButton />
+            <NaverLoginButton />
+        </Wrapper>
+    );
+}
+
+export default LoginButton;
