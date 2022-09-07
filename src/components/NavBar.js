9ed55c228f538a3edd2915da27dd94609e5a29d6
yujiah-github <cil05265@naver.com>
2022-09-07 18:12:16 +0900

    ETC: git pull 충돌 수정

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,13 +6,17 @@ import { MdLocalPharmacy } from 'react-icons/md';
 import { FaHeartbeat } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> 9e246d9 (ETC: git pull 충돌 수정)
+=======
+>>>>>>> d9d94b6 (ETC: git pull 충돌 수정)
 const NavBarWrapper = styled.div`
   background-color: #3c7466;
   padding: 5px;
   text-align: center;
+<<<<<<< HEAD
 <<<<<<< HEAD
   width: 49vh;
   height: 3vw;
@@ -25,15 +29,21 @@ const NavBarWrapper = styled.div`
 =======
   width: 97vw;
   height: 8vh;
+=======
+  width: 49vh;
+  height: 3vw;
+>>>>>>> d9d94b6 (ETC: git pull 충돌 수정)
   display: flex;
   justify-content: space-between;
   justify-items: center;
-
   .item {
     list-style-type: none;
   }
+<<<<<<< HEAD
 
 >>>>>>> 9e246d9 (ETC: git pull 충돌 수정)
+=======
+>>>>>>> d9d94b6 (ETC: git pull 충돌 수정)
   .itemList {
     font-size: 1.8rem;
     text-decoration: none;
@@ -44,6 +54,7 @@ const NavBarWrapper = styled.div`
     text-decoration-line: none;
   }
 `;
+<<<<<<< HEAD
 
 <<<<<<< HEAD
 function NavBar(){
@@ -58,6 +69,8 @@ function NavBar(){
         </NavBarWrapper>
     );
 =======
+=======
+>>>>>>> 2e1215f (ETC: 코드 복붙 (App.js, NavBar.js))
 function NavBar() {
   return (
     <NavBarWrapper>
@@ -68,6 +81,11 @@ function NavBar() {
           </span>
         </Link>
         <Link to={`/pillSearch`}>
+          <li className="itemList">
+            <CgPill />
+          </li>
+        </Link>
+        <Link to={`/pillSearch/detail`}>
           <li className="itemList">
             <CgPill />
           </li>
@@ -86,7 +104,10 @@ function NavBar() {
 >>>>>>> 7528f61 (Feat: Mypage, MonthlyMediger 페이지 추가)
 }
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> 9e246d9 (ETC: git pull 충돌 수정)
+=======
+>>>>>>> d9d94b6 (ETC: git pull 충돌 수정)
 export default NavBar;
