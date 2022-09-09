5bbedfcb878670bb6fc942040a0f1de6c4462baa
yujiah-github <cil05265@naver.com>
2022-09-10 00:16:37 +0900

    ETC: NavBar commit 충돌 수정

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,19 +5,11 @@ import { CgPill } from 'react-icons/cg';
 import { MdLocalPharmacy } from 'react-icons/md';
 import { FaHeartbeat } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> 9e246d9 (ETC: git pull 충돌 수정)
-=======
->>>>>>> d9d94b6 (ETC: git pull 충돌 수정)
 const NavBarWrapper = styled.div`
   background-color: #3c7466;
   padding: 5px;
   text-align: center;
-<<<<<<< HEAD
-<<<<<<< HEAD
   width: 49vh;
   height: 3vw;
   display: flex;
@@ -26,24 +18,7 @@ const NavBarWrapper = styled.div`
   .item {
     list-style-type: none;
   }
-=======
-  width: 97vw;
-  height: 8vh;
-=======
-  width: 49vh;
-  height: 3vw;
->>>>>>> d9d94b6 (ETC: git pull 충돌 수정)
-  display: flex;
-  justify-content: space-between;
-  justify-items: center;
-  .item {
-    list-style-type: none;
-  }
-<<<<<<< HEAD
 
->>>>>>> 9e246d9 (ETC: git pull 충돌 수정)
-=======
->>>>>>> d9d94b6 (ETC: git pull 충돌 수정)
   .itemList {
     font-size: 1.8rem;
     text-decoration: none;
@@ -54,23 +29,7 @@ const NavBarWrapper = styled.div`
     text-decoration-line: none;
   }
 `;
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-function NavBar(){
-    return(
-        <NavBarWrapper>
-            <ul className="item">
-                <Link to={`/`}><span className="itemList"><AiOutlineHome /></span></Link>
-                <Link to={`/pillSearch/detail`}><li className="itemList"><CgPill/></li></Link>
-                <Link to={`/map/hospital`}><li className="itemList"><MdLocalPharmacy/></li></Link>
-                <li className="itemList"><FaHeartbeat/></li>
-            </ul>
-        </NavBarWrapper>
-    );
-=======
-=======
->>>>>>> 2e1215f (ETC: 코드 복붙 (App.js, NavBar.js))
 function NavBar() {
   return (
     <NavBarWrapper>
@@ -101,13 +60,6 @@ function NavBar() {
       </ul>
     </NavBarWrapper>
   );
->>>>>>> 7528f61 (Feat: Mypage, MonthlyMediger 페이지 추가)
-}
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 9e246d9 (ETC: git pull 충돌 수정)
-=======
->>>>>>> d9d94b6 (ETC: git pull 충돌 수정)
+  }
+  
 export default NavBar;
