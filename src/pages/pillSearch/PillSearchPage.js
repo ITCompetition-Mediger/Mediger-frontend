506f21cf60cc36a6502b9f1f585d702c98bb4fc0
yujiah-github <cil05265@naver.com>
2022-09-11 23:19:45 +0900

    Refactor: PillSearchPage에 React Reponsive 적용

--- src/pages/pillSearch/PillSearchPage.js
+++ src/pages/pillSearch/PillSearchPage.js
@@ -4,6 +4,7 @@ import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
 import SearchBar from "../../components/SearchBar";
 import PillSearchList from "./PillSearchList";
+import { Mobile } from "../../components/ReactResponsive";
 
 const Wrapper = styled.form`
     display: flex;
@@ -15,22 +16,24 @@ const PillSearchResultBox = styled.div`
     color: #3C7466;
     padding-bottom: 1rem;
     padding-top: 1.5rem;
-    width: 25vw;
+    width: 70%;
     height: 5vh;
 `;
 
 function PillSearchPage(){
     return(
-        <Wrapper>
-            <Header />
-            <SearchBar />
-            <PillSearchResultBox>
-                <div className="pillTotal">전체 1개</div>
-                <hr className="horizontal"></hr>
-            </PillSearchResultBox>
-            <PillSearchList />
-            <NavBar />
-        </Wrapper>
+        <Mobile>
+            <Wrapper>
+                <Header />
+                <SearchBar />
+                <PillSearchResultBox>
+                    <div className="pillTotal">전체 1개</div>
+                    <hr className="horizontal"></hr>
+                </PillSearchResultBox>
+                <PillSearchList />
+                <NavBar />
+            </Wrapper>
+        </Mobile>
     );
 }
 
