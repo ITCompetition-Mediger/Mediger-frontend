e84c39caf804b096d581fafa6aef81d2bb47e4c8
mongdiwoo <1206jiwoo@gmail.com>
2022-09-15 13:33:39 +0900

    ETC: rebase 오류 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import MonthlyMediger from './myMediger/MonthlyMediger';
 import Mypage from './myMediger/Mypage';
+import DailyMediger from './myMediger/DailyMediger';
+import MedigerList from './medigerList/MedigerList';
 
 function Home() {
   return (
     <div>
       {/* Home!
       <Link to="/"></Link> */}
-      <MonthlyMediger />
+      <Mypage />
     </div>
   );
 }
