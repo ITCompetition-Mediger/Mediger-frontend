26ad6d1fbc7ae168967abbbde71b9859e3881f09
mongdiwoo <1206jiwoo@gmail.com>
2022-09-16 17:30:38 +0900

    ETC: MedigerList에 Layout 컴포넌트 적용

--- src/pages/medigerList/MedigerList.js
+++ src/pages/medigerList/MedigerList.js
@@ -2,84 +2,79 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { IoIosAddCircle } from 'react-icons/io';
 import styled from 'styled-components';
-import Header from '../../components/Header';
-import NavBar from '../../components/NavBar';
 import Medicine from '../../components/Medicine';
 import { Mobile } from '../../components/ReactResponsive';
+import Layout from '../../components/Layout';
 
-// 변수 이름은 무조건 대문자로 시작!
 const All = styled.div`
-  width: 100vw;
-  height: 100vh;
-  margin: 0;
-  padding: 0;
-`;
+  .Content {
+    width: 100vw;
+    height: 65vh;
+    padding: 5vh 0;
 
-const Content = styled.div`
-  width: 100vw;
-  height: 65vh;
-  padding: 5vh 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+  .ListBox {
+    width: 80vw;
+    overflow: auto;
+  }
 
-const ListBox = styled.div`
-  width: 80vw;
-  overflow: auto;
-`;
+  .TitleBox {
+    width: 80vw;
+    height: 10vh;
 
-const TitleBox = styled.div`
-  width: 80vw;
-  height: 10vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
+  .Title {
+    font-size: 20px;
+    font-weight: bold;
 
-const Title = styled.p`
-  font-size: 20px;
-  font-weight: bold;
+    color: #3c7466;
+  }
 
-  color: #3c7466;
-`;
+  .PlusBtn {
+    font-size: 30px;
 
-const PlusBtn = styled.div`
-  font-size: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  background-color: white;
-  color: #3c7466;
+    background-color: white;
+    color: #3c7466;
+  }
 `;
 
 function MedigerList() {
   return (
     <Mobile>
-      <All>
-        <Header></Header>
-        <Content>
-          <TitleBox>
-            <Title>메디저 리스트</Title>
-            <Link to="/">
-              <PlusBtn>
-                <IoIosAddCircle />
-              </PlusBtn>
-            </Link>
-          </TitleBox>
-          <ListBox>
-            <Medicine />
-            <Medicine />
-            <Medicine />
-            <Medicine />
-          </ListBox>
-        </Content>
-        <NavBar />
-      </All>
+      <Layout>
+        <All>
+          <div className="Content">
+            <div className="TitleBox">
+              <p className="Title">메디저 리스트</p>
+              <Link to="/">
+                <div className="PlusBtn">
+                  <IoIosAddCircle />
+                </div>
+              </Link>
+            </div>
+            <div className="ListBox">
+              <Medicine />
+              <Medicine />
+              <Medicine />
+              <Medicine />
+              <Medicine />
+              <Medicine />
+            </div>
+          </div>
+        </All>
+      </Layout>
     </Mobile>
   );
 }
