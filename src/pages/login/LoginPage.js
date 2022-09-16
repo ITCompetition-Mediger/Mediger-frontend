70df125b1398287eec50a3a337a215dc9d80a7cb
mongdiwoo <1206jiwoo@gmail.com>
2022-09-16 18:52:50 +0900

    ETC: LoginPage Header 애니메이션 적용

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,26 +1,22 @@
-import React from "react";
-import KakaoLoginButton from "./KakaoLoginButton";
-import LoginHeader from "./LoginHeader";
-import styled from "styled-components";
-import GoogleLoginButton from "./GoogleLoginButton";
-import NaverLoginButton from "./NaverLoginButton";
-import LoginButton from "./LoginButton";
-import { Mobile } from "../../components/ReactResponsive";
-
+import React from 'react';
+import LoginHeader from './LoginHeader';
+import styled from 'styled-components';
+import LoginButton from './LoginButton';
+import { Mobile } from '../../components/ReactResponsive';
 
 const Wrapper = styled.form`
-    align-items: center;
-    justify-content: center;
+  align-items: center;
+  justify-content: center;
 `;
-function LoginPage(){
-    return(
-        <Mobile>
-            <Wrapper>
-                <LoginHeader />
-                <LoginButton />
-            </Wrapper>
-        </Mobile>
-    );
+function LoginPage() {
+  return (
+    <Mobile>
+      <Wrapper>
+        <LoginHeader />
+        <LoginButton />
+      </Wrapper>
+    </Mobile>
+  );
 }
 
 export default LoginPage;
