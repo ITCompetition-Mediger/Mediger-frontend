fe706224c221ab7f33e41b9060bd497863fdf8fc
mongdiwoo <1206jiwoo@gmail.com>
2022-09-16 15:58:39 +0900

    Edit: Navbar 수정

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,56 +8,108 @@ import { Link } from 'react-router-dom';
 
 const NavBarWrapper = styled.div`
   background-color: #3c7466;
-  width: 100%;
-  height: 6%;
-  padding: 0.5rem;
+  width: 100vw;
+  height: 10vh;
+
   display: flex;
-  justify-content: space-around;
-  justify-items: center;
-  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
 
-  .item {
-    list-style-type: none;
-  }
+  position: fixed;
+  bottom: 0;
+  left: 0;
+`;
 
-  .itemList {
-    font-size: 1.8rem;
-    text-decoration: none;
-    padding: 5%;
-    color: white;
-    display: inline;
-    font-weight: bold;
-    text-decoration-line: none;
-  }
+const Menu = styled.div`
+  font-size: 1.8rem;
+  text-decoration: none;
+  padding: 5%;
+  color: white;
+  display: inline;
+  font-weight: bold;
+  text-decoration-line: none;
 `;
 
 function NavBar() {
   return (
     <NavBarWrapper>
-      <ul className="item">
-        <Link to={`/`}>
-          <span className="itemList">
-            <AiOutlineHome />
-          </span>
-        </Link>
-        <Link to={`/pillSearch`}>
-          <li className="itemList">
-            <CgPill />
-          </li>
-        </Link>
-        <Link to={`/map/pharmacyList`}>
-          <li className="itemList">
-            <MdLocalPharmacy />
-          </li>
-        </Link>
-        <Link to={`/myMediger/Mypage`}>
-          <li className="itemList">
-            <FaHeartbeat />
-          </li>
-        </Link>
-      </ul>
+      <Link to={`/`}>
+        <Menu>
+          <AiOutlineHome />
+        </Menu>
+      </Link>
+
+      <Link to={`/pillSearch`}>
+        <Menu>
+          <CgPill />
+        </Menu>
+      </Link>
+
+      <Link to={`/map/pharmacyList`}>
+        <Menu>
+          <MdLocalPharmacy />
+        </Menu>
+      </Link>
+
+      <Link to={`/myMediger/Mypage`}>
+        <Menu>
+          <FaHeartbeat />
+        </Menu>
+      </Link>
     </NavBarWrapper>
   );
 }
 
 export default NavBar;
+
+// const NavBarWrapper = styled.div`
+//   background-color: #3c7466;
+//   width: 100vw;
+//   height: 10vh;
+
+//   display: flex;
+//   justify-content: space-evenly;
+//   justify-items: center;
+//   flex-direction: column;
+
+//   .item {
+//     list-style-type: none;
+//   }
+
+//   .itemList {
+//     font-size: 1.8rem;
+//     text-decoration: none;
+//     padding: 5%;
+//     color: white;
+//     display: inline;
+//     font-weight: bold;
+//     text-decoration-line: none;
+//   }
+// `;
+
+{
+  /* <NavBarWrapper>
+<ul className="item">
+  <Link to={`/`}>
+    <span className="itemList">
+      <AiOutlineHome />
+    </span>
+  </Link>
+  <Link to={`/pillSearch`}>
+    <li className="itemList">
+      <CgPill />
+    </li>
+  </Link>
+  <Link to={`/map/pharmacyList`}>
+    <li className="itemList">
+      <MdLocalPharmacy />
+    </li>
+  </Link>
+  <Link to={`/myMediger/Mypage`}>
+    <li className="itemList">
+      <FaHeartbeat />
+    </li>
+  </Link>
+</ul>
+</NavBarWrapper> */
+}
