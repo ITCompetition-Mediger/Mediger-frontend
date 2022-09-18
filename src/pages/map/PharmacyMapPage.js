adf3e3a65d76908eb7855311d93f9cea99f9293e
yujiah-github <cil05265@naver.com>
2022-09-18 13:03:07 +0900

    Design: PharmacyMapPage에 DesktopView 적용

--- src/pages/map/PharmacyMapPage.js
+++ src/pages/map/PharmacyMapPage.js
@@ -1,7 +1,9 @@
 import React from "react";
 import styled from "styled-components";
+import DesktopView from "../../components/DesktopView";
 import Header from "../../components/Header";
 import NavBar from "../../components/NavBar";
+import { Desktop, Mobile } from "../../components/ReactResponsive";
 
 const Wrapper = styled.form`
     display: flex;
@@ -16,11 +18,18 @@ const DummyData = styled.div`
 
 function PharmacyMapPage(){
     return(
+        <div>
+        <Mobile>
         <Wrapper>
             <Header />
             <DummyData>지도 api 표시</DummyData>
             <NavBar />
         </Wrapper>
+        </Mobile>
+        <Desktop>
+            <DesktopView />
+        </Desktop>
+        </div>
     );
 }
 
