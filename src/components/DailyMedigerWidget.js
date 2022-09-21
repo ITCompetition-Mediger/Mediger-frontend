58dcb46d453534fb57b8446ddccaa66fed1c037a
mongdiwoo <1206jiwoo@gmail.com>
2022-09-21 11:16:38 +0900

    ETC: 그림자 수정

--- src/components/DailyMedigerWidget.js
+++ src/components/DailyMedigerWidget.js
@@ -21,7 +21,7 @@ const DailyWidget = styled.div`
     align-items: center;
     justify-content: center;
 
-    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    /* box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px; */
   }
 
   .TimeTitle {
