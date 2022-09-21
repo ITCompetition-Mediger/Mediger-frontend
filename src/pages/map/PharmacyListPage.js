862a6cc64095c1640597a8cb284c1822d34432df
mongdiwoo <1206jiwoo@gmail.com>
2022-09-21 10:17:41 +0900

    ETC: PharmacyListPage Layout 적용

--- src/pages/map/PharmacyListPage.js
+++ src/pages/map/PharmacyListPage.js
@@ -1,65 +1,130 @@
-import React from "react";
-import styled from "styled-components";
-import Header from "../../components/Header";
-import NavBar from "../../components/NavBar";
-import PharmacyList from "./PharmacyList";
-import { Desktop, Mobile, Tablet } from "../../components/ReactResponsive";
-import DesktopView from "../../components/DesktopView";
+import React from 'react';
+import styled from 'styled-components';
+import Layout from '../../components/Layout';
+import PharmacyList from './PharmacyList';
+import { Desktop, Mobile, Tablet } from '../../components/ReactResponsive';
+import DesktopView from '../../components/DesktopView';
 import TabletView from '../../components/TabletView';
 
 const Wrapper = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  width: 85vw;
+  height: 75vh;
 
-    .nameHeader {
-        width: 50%;
-        text-align: left;
-        margin-top: 1.5rem;
-        margin-bottom: 1.5rem;
-        font-size: 1.5rem;
-        color: #3A6C60;
-        font-weight: bold;
-        padding-right: 5rem;
-    }
+  p {
+    margin: 0;
+    padding: 0;
+  }
+
+  .nameHeader {
+    margin: 4vh 0;
+  }
+  .title {
+    color: #3c7466;
+    font-weight: bolder;
+    font-size: 5vw;
+  }
 `;
 
 const PharmacyListComponentWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
+  background-color: aliceblue;
+  height: 63vh;
+  overflow: scroll;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-
-function PharmacyListPage(){
-    return(
-        <div>
-            <Mobile>
-                <Wrapper>
-                    <Header />
-                    <div className="nameHeader">
-                        💊 현위치 주변 약국
-                    </div>
-                    <PharmacyListComponentWrapper>
-                        <PharmacyList />
-                        <PharmacyList />
-                        <PharmacyList />
-                        <PharmacyList />
-                        <PharmacyList />
-                        <PharmacyList />
-                        <NavBar />
-                    </PharmacyListComponentWrapper>
-                </Wrapper>
-            </Mobile>
-            <Desktop>
-                <DesktopView />
-            </Desktop>
-            <Tablet>
-                <TabletView />
-            </Tablet>
-        </div>
-    );
+function PharmacyListPage() {
+  return (
+    <div>
+      <Mobile>
+        <Layout>
+          <Wrapper>
+            <div className="nameHeader">
+              <p className="title">💊 현위치 주변 약국</p>
+            </div>
+            <PharmacyListComponentWrapper>
+              <PharmacyList />
+              <PharmacyList />
+              <PharmacyList />
+            </PharmacyListComponentWrapper>
+          </Wrapper>
+        </Layout>
+      </Mobile>
+      <Desktop>
+        <DesktopView />
+      </Desktop>
+      <Tablet>
+        <TabletView />
+      </Tablet>
+    </div>
+  );
 }
 
 export default PharmacyListPage;
+
+// import React from "react";
+// import styled from "styled-components";
+// import Header from "../../components/Header";
+// import NavBar from "../../components/NavBar";
+// import PharmacyList from "./PharmacyList";
+// import { Desktop, Mobile, Tablet } from "../../components/ReactResponsive";
+// import DesktopView from "../../components/DesktopView";
+// import TabletView from '../../components/TabletView';
+
+// const Wrapper = styled.form`
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+
+//     .nameHeader {
+//         width: 50%;
+//         text-align: left;
+//         margin-top: 1.5rem;
+//         margin-bottom: 1.5rem;
+//         font-size: 1.5rem;
+//         color: #3A6C60;
+//         font-weight: bold;
+//         padding-right: 5rem;
+//     }
+// `;
+
+// const PharmacyListComponentWrapper = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     width: 100%;
+// `;
+
+// function PharmacyListPage(){
+//     return(
+//         <div>
+//             <Mobile>
+//                 <Wrapper>
+//                     <Header />
+//                     <div className="nameHeader">
+//                         💊 현위치 주변 약국
+//                     </div>
+//                     <PharmacyListComponentWrapper>
+//                         <PharmacyList />
+//                         <PharmacyList />
+//                         <PharmacyList />
+//                         <PharmacyList />
+//                         <PharmacyList />
+//                         <PharmacyList />
+//                         <NavBar />
+//                     </PharmacyListComponentWrapper>
+//                 </Wrapper>
+//             </Mobile>
+//             <Desktop>
+//                 <DesktopView />
+//             </Desktop>
+//             <Tablet>
+//                 <TabletView />
+//             </Tablet>
+//         </div>
+//     );
+// }
+
+// export default PharmacyListPage;
