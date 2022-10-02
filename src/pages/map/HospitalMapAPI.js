b22808f23e2b7f0fa0ca21e37bbd638686c4f1cb
yujiah-github <cil05265@naver.com>
2022-10-02 16:51:16 +0900

    Refactor: 카카오 api 이용하여 지도 불러오는 로직 추가

--- src/pages/map/HospitalMapAPI.js
+++ src/pages/map/HospitalMapAPI.js
@@ -3,27 +3,31 @@ import React, {useEffect} from "react";
 const { kakao } = window
 
 function HospitalMapAPI(){
+    const [controller, setController] = useState();
     useEffect(() => {
-            const mapContainer = document.getElementById('hospitalMap')
-            const mapOption = {
+        //마커를 클릭하면 장소명을 표출할 윈포윈도우
+        let infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+        function locPositionAPI(){
+            // HTML5의 geolocaiton으로 사용할 수 있는지 확인
+            // GeoLocation을 이용해서 접속 위치를 받기
+            navigator.geolocation.getCurrentPosition(function(position){
+                let lat = position.coords.latitude, // 위도
+                    lon = position.coords.longitude; // 경도
+                let locPosition = new kakao.maps.LatLng(lat, lon);
+                return locPosition; //지도에 위도 경도 반환
+            });
+        }
+
+            let mapContainer = document.getElementById('hospitalMap')
+            let mapOption = {
                 center: new kakao.maps.LatLng(37.566826, 126.9786567),
                 level: 3,
             }
 
-            const map = new kakao.maps.Map(mapContainer, mapOption);
+            let map = new kakao.maps.Map(mapContainer, mapOption);
 
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                let lat = position.coords.latitude,
-                    lon = position.coords.longitude; 
-                
-                var locPosition = new kakao.maps.LatLng(lat, lon)
-                map.setCenter(locPosition);   
-                });
-            }
+            const ps = new kakao.maps.services.Places(); 
 
-        const ps = new kakao.maps.services.Places(); 
-        var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 })
         ps.keywordSearch('병원', placesSearchCB);
         
         function placesSearchCB(data, status){
@@ -52,10 +56,21 @@ function HospitalMapAPI(){
             infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
             infowindow.open(map, marker);
         });
+    setController(map);
     }},[]);
 
+    // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수
+    function zoomIn() {
+            controller.setLevel(controller.getLevel() - 1);
+        }
+
+        // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수
+    function zoomOut() {
+            controller.setLevel(controller.getLevel() + 1);
+        }
 
     return(
+        <>
             <div
                 id="hospitalMap"
                 style={{
@@ -66,6 +81,17 @@ function HospitalMapAPI(){
                 }}
             >
             </div>
+            <div className="radiusBorder"> 
+                    <span onClick={zoomIn}>
+                        <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_plus.png" alt="확대">
+                        </img>
+                    </span>  
+                    <span onClick={zoomOut}>
+                        <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png" alt="축소">
+                        </img>
+                    </span>
+                </div>
+         </>       
     );
 }
 
