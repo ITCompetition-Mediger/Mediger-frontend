4d2bc9ace7346f56e680128d294f99b4ab308864
mongdiwoo <1206jiwoo@gmail.com>
2022-10-06 01:30:09 +0900

    ETC: 주변 약국 검색

--- src/pages/map/PharmacyMapAPI.js
+++ src/pages/map/PharmacyMapAPI.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 
 let lat, lng;
 
@@ -13,72 +14,62 @@ navigator.geolocation.getCurrentPosition(onGeoOk);
 const { kakao } = window;
 
 function PharmacyMapAPI() {
-  //   useEffect(() => {
-  //     const mapContainer = document.getElementById('pharmacyMap');
-  //     const mapOption = {
-  //       center: new kakao.maps.LatLng(37.566826, 126.9786567),
-  //       level: 3,
-  //     };
-
-  //     const map = new kakao.maps.Map(mapContainer, mapOption);
-
-  //     if (navigator.geolocation) {
-  //       navigator.geolocation.getCurrentPosition(function (position) {
-  //         let lat = position.coords.latitude,
-  //           lon = position.coords.longitude;
-
-  //         var locPosition = new kakao.maps.LatLng(lat, lon);
-  //         map.setCenter(locPosition);
-  //       });
-  //     }
-
-  //     const ps = new kakao.maps.services.Places();
-  //     var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-  //     ps.keywordSearch('약국', placesSearchCB);
-
-  //     function placesSearchCB(data, status) {
-  //       if (status === kakao.maps.services.Status.OK) {
-  //         let bounds = new kakao.maps.LatLngBounds();
-
-  //         for (let i = 0; i < data.length; i++) {
-  //           displayMarker(data[i]);
-  //           bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-  //         }
-
-  //         map.setBounds(bounds);
-  //       }
-  //     }
-
-  //     function displayMarker(place) {
-  //       let marker = new kakao.maps.Marker({
-  //         map: map,
-  //         position: new kakao.maps.LatLng(place.y, place.x),
-  //       });
-
-  //       kakao.maps.event.addListener(marker, 'click', function () {
-  //         infowindow.setContent(
-  //           '<div style="padding:5px;font-size:12px;">' +
-  //             place.place_name +
-  //             '</div>',
-  //         );
-  //         infowindow.open(map, marker);
-  //       });
-  //     }
-  //   }, []);
   useEffect(() => {
     console.log(lat, lng);
+
+    var infowindow = new kakao.maps.InfoWindow({ zindex: 1 });
+
     var container = document.getElementById('pharmacyMap');
     var options = {
       center: new kakao.maps.LatLng(lat, lng),
-      level: 3,
+      level: 5,
     };
     var map = new kakao.maps.Map(container, options);
+    const ps = new kakao.maps.services.Places(map);
 
-    var markerPosition = new kakao.maps.LatLng(lat, lng);
-    var marker = new kakao.maps.Marker({
-      position: markerPosition,
-    });
-    marker.setMap(map);
+    ps.categorySearch('PM9', placesSearchCB, { useMapBounds: true });
+
+    function placesSearchCB(data, status, pagination) {
+      //   if (status === kakao.maps.services.Status.OK) {
+      //     let bounds = new kakao.maps.LatLngBounds();
+
+      //     for (let i = 0; i < data.length; i++) {
+      //       displayMarker(data[i]);
+      //       bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+      //     }
+
+      //     map.setBounds(bounds);
+      //   }
+      if (status === kakao.maps.services.Status.OK) {
+        for (var i = 0; i < data.length; i++) {
+          displayMarker(data[i]);
+        }
+      }
+    }
+
+    function displayMarker(place) {
+      let marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x),
+      });
+
+      kakao.maps.event.addListener(marker, 'click', function () {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent(
+          '<div style="padding:5px;font-size:12px;">' +
+            place.place_name +
+            '</div>',
+        );
+        infowindow.open(map, marker);
+      });
+    }
+
+    // 현위치 마커
+    // var markerPosition = new kakao.maps.LatLng(lat, lng);
+    // var marker = new kakao.maps.Marker({
+    //   position: markerPosition,
+    // });
+    // marker.setMap(map);
   }, []);
 
   return (
