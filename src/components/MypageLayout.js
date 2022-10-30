073041bc2c7c1c14fcd6960e6d1e6136060451fa
yujiah-github <cil05265@naver.com>
2022-10-30 15:18:54 +0900

    Refactor: 필요없는 소스코드 삭제

--- src/components/MypageLayout.js
+++ src/components/MypageLayout.js
@@ -33,27 +33,3 @@ const Layout = (props) => {
 };
 
 export default Layout;
-
-// import React from 'react';
-// import NavBar from '../components/NavBar';
-// import styled from 'styled-components';
-
-// const Main = styled.div`
-//   width: 100vw;
-//   height: 100vh;
-
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
-
-// const Layout = (props) => {
-//   return (
-//     <div>
-//       <Main>{props.children}</Main>
-//       <NavBar />
-//     </div>
-//   );
-// };
-
-// export default Layout;
