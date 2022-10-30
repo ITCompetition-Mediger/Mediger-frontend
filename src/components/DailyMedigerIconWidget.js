1246abf46f56faecbb84dd84480f3396f945b826
yujiah-github <cil05265@naver.com>
2022-10-30 15:20:07 +0900

    Refactor: 필요없는 소스코드 삭제

--- src/components/DailyMedigerIconWidget.js
+++ src/components/DailyMedigerIconWidget.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import styled from 'styled-components';
 import notFoundImg from '../images/notFoundImg.png';
 
 const IconWidget = styled.div`
-  /* width: 7vw;
-  height: 7vw;
-  margin: 1vw;
-  border-radius: 50%;
-  background-color: white; */
 
   img {
     width: 9vw;
